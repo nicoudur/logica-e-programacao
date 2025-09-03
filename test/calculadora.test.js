40deeacc0b42e99f41c8de169836f6d5dcf49d67
Nicolas Duarte <nicolascoutoduarte@gmail.com>
2025-09-02 23:00:16 -0300

    adicionei testes automatizados a função

---
+++ test/calculadora.test.js
@@ -0,0 +1,19 @@
+const { somarDoisNumeros } = require('../src/calculadora');
+const { expect } = require('chai');
+
+describe('Testes da Função de Soma', function () {
+
+    it('A função deve ser capaz de somar dois número positivos', function () {
+
+        const resultadoDaSoma = somarDoisNumeros(5, 3);
+
+        expect(resultadoDaSoma).to.equal(8);
+    });
+
+    it('A função deve ser capaz de somar um número positivo e um negativo', function () {
+
+        const resultadoDaSoma = somarDoisNumeros(50, -15);
+
+        expect(resultadoDaSoma).to.equal(35);
+    });
+})
